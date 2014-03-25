c3c891f6972e9fa1a894a6ccf7482e5da0a43c5a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-03-25 10:54:33 +0100

    Clean.

--- source/Foo/Bar.php
+++ source/Foo/Bar.php
@@ -16,6 +16,9 @@ class Bar {
      */
     public function isAllowed ( $email ) {
 
+        if(0 == preg_match('/[a-z]+\.[a-z]+@[a-z]\.(com|net|org|hl)/', $email))
+            throw new \Exception('Damn…');
+
         return true;
     }
 }

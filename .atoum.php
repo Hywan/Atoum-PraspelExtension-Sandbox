390ce334db44a6de55bc3eb42fdadb86f64926de
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-03-25 11:00:09 +0100

    Switch to `experimentation` branch of our fork.

--- .atoum.php
+++ .atoum.php
@@ -2,4 +2,9 @@
 
 require_once 'vendor/autoload.php';
 
+\mageekguy\atoum\instrumentation\stream\cache::setCacheDirectory('/tmp');
+
 $runner->addExtension(new Atoum\PraspelExtension\Manifest());
+$runner->disableXDebugCodeCoverage();
+$runner->enableInstrumentation()
+       ->disableMoleInstrumentation();

f8731d3c4a9f44c123b848a4978a22fc50b77ed9
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-03-25 14:58:49 +0100

    Add two examples: regex and array.

--- tests/units/Foo.php
+++ tests/units/Foo.php
@@ -66,4 +66,51 @@ class stdClass extends \atoum\test {
                  ->isEqualTo(JSON_ERROR_NONE);
         }
     }
+
+    public function testRegex ( ) {
+
+        $foo = $this->realdom->regex('/.+\sfoo\s.+/');
+
+        foreach($this->sampleMany($foo, 100) as $data)
+            $this->string($data)
+                 ->contains('foo');
+    }
+
+    public function testArray ( ) {
+
+        // in Praspel:
+        //     array([from … to …, from … to …], 7)
+
+        // in PHP, let's try to represent:
+        //     array(
+        //         [
+        //             to boolean(),
+        //             from integer() to array([to string('a', 'e', 1)], 3)
+        //         ],
+        //         7
+        //     )
+        $array = $this->realdom->array(
+            array(
+                array(
+                    $this->realdom->natural(0, 1),
+                    $this->realdom->boolean()
+                ),
+                array(
+                    $this->realdom->integer(),
+                    $this->realdom->array(
+                        array(
+                            array(
+                                $this->realdom->natural(0, 1),
+                                $this->realdom->string('a', 'e', 1)
+                            )
+                        ),
+                        3
+                    )
+                )
+            ),
+            7
+        );
+
+        $this->array($this->sample($array));
+    }
 }
